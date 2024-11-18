390087023538c4487921b31bbfc1a11dd85ba59e
Park <psjj4355@naver.com>
2024-11-18 20:02:07 +0900

    메인 내용 추가

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
 
-// 템플릿 파일 위치 지정
-app.set('views', path.join(__dirname, 'templates'));
+// 'static' 폴더를 정적 파일 경로로 설정
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
-// 정적 파일을 제공할 때 사용할 public 폴더 경로 설정
-app.use(express.static(path.join(__dirname, 'public')));
-
-// HTML 파일을 렌더링하는 라우트
+// HTML 파일 제공
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+    res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 });
 
 // 서버 시작
